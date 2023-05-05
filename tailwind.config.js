1ecb785043d7bd488fdd67df229d53f7dd5f1d1c
JOYBOY-0 <falamino00@gmail.com>
2023-05-05 00:21:47 -0300

    add next-viewport utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        ['red-will']: '#FB073B',
-        ['dark-void']: '#131211'
+        ['red-will']: 'var(--color-red-will)',
+        ['dark-void']: 'var(--color-dark-void)'
       },
       fontFamily: {
         sans: 'var(--font-sans)',
         serif: 'var(--font-serif)',
         glare: 'var(--font-glare)',
         text: 'var(--font-text)'
+      },
+      width: {
+        ['full-vw']: 'calc(var(--vw) * 100)'
+      },
+      height: {
+        ['full-vh']: 'calc(var(--vh) * 100)'
       }
     }
   },
